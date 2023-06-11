ce0247a1dedf00a372741773229d7cfcb1aa8f97
JongHyuckLee <yarn add --dev --exact prettier>
2023-06-12 01:33:12 +0900

    git section test

--- 실습예제/wall3d.js
+++ 실습예제/wall3d.js
@@ -2,6 +2,7 @@
         console.log(document.body.offsetHeight , window.innerHeight)
         const stageElem = document.querySelector('.stage');
         const houseElem = document.querySelector('.house');
+        const worldElem = document.querySelector('.world');
         const barElem = document.querySelector('.progress-bar');
         const mousePos = { x: 0, y: 0 };
         let maxScrollValue = 0;
@@ -10,12 +11,19 @@
         };
         window.addEventListener('scroll', function () {
             // console.log(scrollY/maxScrollValue);
-            console.log({scrollY, maxScrollValue});
-            const scrollPer = scrollY / maxScrollValue; // 전체 스크롤 비율
-            const zMove = scrollPer * 970; // z 좌표로 움직이는 값, 970은 너무 작은 비율로 움직이기 때문에 곱해주는 보완값
-            houseElem.style.transform = `translateZ(${zMove - 490}vw)`; // z 좌표 즉, 화면 안쪽으로 이동 시킴
+            // console.log({scrollY, maxScrollValue});
+            const scrollPer = (scrollY) / maxScrollValue; // 전체 스크롤 비율
+
             // progress bar
             barElem.style.width = `${scrollPer * 100}%`;
+            console.log(scrollY, window.innerHeight)
+            if(scrollY >= window.innerHeight && scrollY <= window.innerHeight * 9) {
+                const zMove = scrollPer * 970; // z 좌표로 움직이는 값, 970은 너무 작은 비율로 움직이기 때문에 곱해주는 보완값
+                houseElem.style.transform = `translateZ(${zMove - 590}vw)`; // z 좌표 즉, 화면 안쪽으로 이동 시킴
+                worldElem.style.top = 0;
+            } else {
+                worldElem.style.top ='-100vh'
+            }
         });
 
         window.addEventListener("mousemove", function (e) {
@@ -24,7 +32,7 @@
 
             mousePos.x = -1 + (e.clientX / window.innerHeight) * 2;
             mousePos.y = 1 - (e.clientY / window.innerHeight) * 2;
-            stageElem.style.transform = `rotateX(${mousePos.y * 7}deg) rotateY(${mousePos.x * 7}deg)`
+            stageElem.style.transform = `rotateX(${mousePos.y * 10}deg) rotateY(${mousePos.x * 10}deg)`
             // console.log(e.clientX / window.innerWidth, e.clientY / window.innerHeight)
         });
 
